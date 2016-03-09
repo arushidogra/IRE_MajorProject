232780c1525c8dd9d9a3701ad9596655bb0afa29
arushidogra <arushidogra1995@gmail.com>
2016-03-09 12:03:53 +0530

    StopWords code added

---
+++ stop_words.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+'''
+Stop words for hindi
+I am taking top 50 words
+'''
+from HindiTokenizer import Tokenizer
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: " + sys.argv[0] + " <corpusfile>\n")
+        sys.exit(2)
+    file_name = sys.argv[1]
+    fopen = open(file_name, "r")
+    a = open("output_stopwords.txt", "w")
+    dic_tok = {}
+    while True:
+        line = fopen.readline()[0:-1]
+        if line == '':
+            break
+        else:
+            t = Tokenizer(line)
+            t.tokenize()
+            for i in t.print_tokens():
+                try:
+                    dic_tok[i] += 1
+                except KeyError:
+                    dic_tok[i] = 1
+    final_list=[]
+    for i in dic_tok.items():
+        i = list(i)
+        i.reverse()
+        final_list.append(i)
+    final_list.sort()
+    final_list.reverse()
+    final_list=final_list[0:50]
+    for i in final_list:
+        a.write(i[1]+"\n")
+
+    a.close()
+    fopen.close()
