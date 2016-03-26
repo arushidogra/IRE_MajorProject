85d67f2d2d273097e1615bb032655b15032d27c6
arushidogra <arushidogra1995@gmail.com>
2016-03-26 22:56:20 +0530

    changes

--- stop_words.py
+++ stop_words.py
@@ -7,11 +7,11 @@ from HindiTokenizer import Tokenizer
 import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        sys.stderr.write("Usage: " + sys.argv[0] + " <corpusfile>\n")
+        sys.stderr.write("Usage: " + sys.argv[0] + " <corpusfile> <outputfile>\n")
         sys.exit(2)
     file_name = sys.argv[1]
     fopen = open(file_name, "r")
-    a = open("output_stopwords.txt", "w")
+    a = open(sys.argv[2], "w")
     dic_tok = {}
     while True:
         line = fopen.readline()[0:-1]
