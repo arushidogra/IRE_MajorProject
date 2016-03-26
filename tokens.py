40327dcd69e3d0ba77689e6f2729a454fae58798
arushidogra <arushidogra1995@gmail.com>
2016-03-26 22:51:49 +0530

    changes

--- tokens.py
+++ tokens.py
@@ -6,21 +6,20 @@ from HindiTokenizer import Tokenizer
 import sys
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        sys.stderr.write("Usage: " + sys.argv[0] + " <corpusfile>\n")
+        sys.stderr.write("Usage: " + sys.argv[0] + " <corpusfile> <outputfile>\n")
         sys.exit(2)
     file_name = sys.argv[1]
     fopen = open(file_name, "r")
-    a = open("output_sentences.txt", "w")
+    a = open(sys.argv[2], "w")
     while True:
         line = fopen.readline()[0:-1]
         if line == '':
             break
         else:
             t = Tokenizer(line)
-            t.generate_sentences()
-            for i in t.print_sentences():
+            t.tokenize()
+            for i in t.print_tokens():
                 a.write(i+"\n")
-                
 
     a.close()
     fopen.close()
