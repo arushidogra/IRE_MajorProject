22d86d50eaa77f3aff8ff3404375f97988a294d7
deepanshu1995 <deepanshuvijay01995@gmail.com>
2016-04-08 02:22:20 +0530

    Divide Data into Train and Test for Categoziation

---
+++ Categoriser/divide_data.py
@@ -0,0 +1,22 @@
+#1600 400
+import json
+
+train = open("train_data.dat","w")
+test = open("test_data.dat","w")
+
+
+filename = open("hi.data","r")
+start = 0
+
+while True:
+    line = filename.readline()
+    if line == '' or start>2000:
+        break
+    if start < 1600:
+        train.write(line+"\n")
+    elif start < 2000:
+        test.write(line+"\n")
+    start = start + 1
+filename.close()
+train.close()
+test.close()
